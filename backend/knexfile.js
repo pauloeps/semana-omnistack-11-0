66f7cd2722f5ebfb3ef64a156598080ca4157a62
Paulo <paulopsslmn@gmail.com>
2020-03-27 12:11:27 -0300

    Corrigido erros no knexfile.js

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -10,11 +10,7 @@ module.exports = {
     migrations: {
       directory: './src/database/migrations'
     },
-<<<<<<< HEAD
-	useNullAsDefault: true,
-=======
-    useNullAsDefault: true,
->>>>>>> 66703f7cf42ce090ed89e7ef86aa770b3a33d8da
+	  useNullAsDefault: true,
   },
   
 
